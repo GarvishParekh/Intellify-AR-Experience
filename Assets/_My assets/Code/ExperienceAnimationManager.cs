1c2bd2e6d1e89ab725f65bee031caddf2b583f88
Garvish Parekh <garvish.p@theintellify.com>
2024-09-09 12:04:14 +0530

    All companies experience completed

--- Assets/_My assets/Code/ExperienceAnimationManager.cs
+++ Assets/_My assets/Code/ExperienceAnimationManager.cs
@@ -1,6 +1,8 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 
 public class ExperienceAnimationManager : MonoBehaviour
 {
@@ -17,6 +19,11 @@ public class ExperienceAnimationManager : MonoBehaviour
     [SerializeField] private Image categoryLine;
     [SerializeField] private Image functionLine;
 
+    [SerializeField] private float lineFillValueMultiplier;
+
+    [SerializeField] private List<Image> glowImageList = new List<Image>();
+    [SerializeField] private float glowFillValueMultiplier;
+
     [Space]
     [SerializeField] private CanvasGroup productGradient;
     [SerializeField] private CanvasGroup categoryGradient;
@@ -24,6 +31,20 @@ public class ExperienceAnimationManager : MonoBehaviour
 
     [Space]
     [SerializeField] private CanvasGroup tapHereText;
+    [SerializeField] private GameObject blurImage;
+
+    [Space]
+    [SerializeField] private Color productColor;
+    [SerializeField] private Image informationDivider;
+    [SerializeField] private TMP_Text userCaseText;
+
+    private void Start()
+    {
+        informationDivider.color = productColor;
+        userCaseText.color = productColor;
+
+        tapHereText.GetComponent<TMP_Text>().text = "View";
+    }
 
     private void ResetAnimation()
     {
@@ -36,56 +57,77 @@ public class ExperienceAnimationManager : MonoBehaviour
         productLine.fillAmount = 0;
         categoryLine.fillAmount = 0;
         functionLine.fillAmount = 0;
+
+        mainHolderRectTransform.sizeDelta = initialPosition;
+        informationHolder.alpha = 0;    
+
+        foreach (Image glowImage in glowImageList) 
+        {
+            CanvasGroup cg = glowImage.GetComponent<CanvasGroup>();
+            cg.alpha = 0;
+            glowImage.fillAmount = 0;
+        }
     }
 
+    bool isPlayingAnimation = false;
     private IEnumerator StartAnimation()
     { 
+        isPlayingAnimation = true;
         ResetAnimation();
         LeanTween.alphaCanvas(mainHolder, 1, 0.25f).setEaseInOutSine();
 
         yield return new WaitForSeconds(1);
 
-        float productLineFillVlaue = 0;
-        while (productLineFillVlaue < 1)
+        float lineImageFillValue = 0;
+        while (lineImageFillValue < 1)
         {
-            productLineFillVlaue += Time.deltaTime;
-            productLine.fillAmount = productLineFillVlaue;
+            lineImageFillValue += Time.deltaTime * lineFillValueMultiplier;
+
+            productLine.fillAmount = lineImageFillValue;
+            categoryLine.fillAmount = lineImageFillValue;
+            functionLine.fillAmount = lineImageFillValue;
             yield return null;
         }
-        LeanTween.alphaCanvas(productGradient, 1, 0.25f).setEaseInOutSine();
+        LeanTween.alphaCanvas(productGradient, 1, 0.8f).setEaseInOutSine();
+        LeanTween.alphaCanvas(categoryGradient, 1, 0.8f).setEaseInOutSine();
+        LeanTween.alphaCanvas(functionGradient, 1, 0.8f).setEaseInOutSine();
 
-        yield return new WaitForSeconds(1);
-
-        float categoryLineFillVlaue = 0;
-        while (categoryLineFillVlaue < 1)
+        //yield return new WaitForSeconds(1);
+        float glowImageFillValue = 0;
+        while (glowImageFillValue < 1)
         {
-            categoryLineFillVlaue += Time.deltaTime;
-            categoryLine.fillAmount = categoryLineFillVlaue;
+            glowImageFillValue += Time.deltaTime * glowFillValueMultiplier;
+            foreach (Image glowImage in glowImageList) 
+            {
+                CanvasGroup cg = glowImage.GetComponent<CanvasGroup>();
+                cg.alpha = 1;
+                glowImage.fillAmount = glowImageFillValue;
+            }
             yield return null;
         }
-        LeanTween.alphaCanvas(categoryGradient, 1, 0.25f).setEaseInOutSine();
-
-        yield return new WaitForSeconds(1);
-        
-        float functionLineFillVlaue = 0;
-        while (functionLineFillVlaue < 1)
+        foreach (Image glowImage in glowImageList)
         {
-            functionLineFillVlaue += Time.deltaTime;
-            functionLine.fillAmount = functionLineFillVlaue;
-            yield return null;
+            CanvasGroup cg = glowImage.GetComponent<CanvasGroup>();
+            LeanTween.alphaCanvas(cg , 0, 2f).setEaseInOutSine().setOnComplete(()=>
+            {
+                LeanTween.alphaCanvas(cg, 1, 2f).setEaseInOutSine().setLoopPingPong(1);
+            });
         }
-        LeanTween.alphaCanvas(functionGradient, 1, 0.25f).setEaseInOutSine();
 
         yield return new WaitForSeconds(1);
         LeanTween.alphaCanvas(tapHereText, 1, 0.25f).setEaseInOutSine();
 
         yield return new WaitForSeconds(0.5f);
-        LeanTween.alphaCanvas(tapHereText, 0, 0.25f).setEaseInOutSine().setLoopPingPong();
+        LeanTween.alphaCanvas(tapHereText, 0, 1f).setEaseInOutSine().setLoopPingPong();
+
+        isPlayingAnimation = false;
     }
 
     bool isOpen = false;
     public void _OpenAnimation()
     {
+        if (isPlayingAnimation) return;
+
         if (!isOpen)
         {
             LeanTween.alphaCanvas(tapHereText, 0, 0.25f).setEaseInOutSine();
@@ -100,27 +142,50 @@ public class ExperienceAnimationManager : MonoBehaviour
             LeanTween.alphaCanvas(informationHolder, 0, 0.25f).setEaseInOutSine().setOnComplete(() =>
             {
                 LeanTween.size(mainHolderRectTransform, initialPosition, 0.25f).setEaseInOutSine();
-                LeanTween.alphaCanvas(tapHereText, 1, 0.25f).setEaseInOutSine();
                 isOpen = false;
             });
         }
     }
 
-    public void _CloseAnimation()
-    {
-        
-    }
-
     bool onceSeen = false;
+    [ContextMenu("Play animation")]
     public void _OnSeen()
     {
-        if (onceSeen) return;
+        //if (onceSeen) return;
+
+        if (isPlayingAnimation)
+        {
+            StopCoroutine(nameof(StartAnimation));  
+        }
         StartCoroutine(nameof(StartAnimation));
-        onceSeen = true;
+
+        //onceSeen = true;
     }
 
     public void _OnNotSeen()
     {
 
     }
+
+    private IEnumerator UseCaseGowingEffect()
+    {
+        while(true)
+        {
+
+            yield return null;
+        }
+    }
+
+
+    //#TESTING
+    bool isImageOn = true;
+    public void _BlurImageSwitch()
+    {
+        if (isImageOn) isImageOn = false;
+        else isImageOn = true;
+
+        blurImage.SetActive(isImageOn);
+    }
 }
+
+
